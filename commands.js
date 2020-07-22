47c0ee03690e09499daeb03d633e098a8674c84d
UOP <TheMezStrikes@gmail.com>
2020-07-22 10:12:24 +0000

    Remove reference to deleted file

--- commands.js
+++ commands.js
@@ -1,4 +1,3 @@
-global.Banlist = JSON.parse(FS.readFileSync('data/banlist.json'));
 global.PokeDex = require('./data/pokedex.js');
 global.fdata = require('./data/formats-data.js');
 global.Items = require('./data/items.js');
